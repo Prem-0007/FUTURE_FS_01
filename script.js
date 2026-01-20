a4bfb98c052443022d2238d19273bc37567c5158
Prem <premkumarballa777@gmail.com>
2026-01-20 16:09:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ function onScroll() {
   
   let currentSectionId = "";
 
-  // 1. REVEAL ANIMATIONS
   reveals.forEach(el => {
     const rect = el.getBoundingClientRect();
     if (rect.top < windowHeight - 100) {
@@ -17,27 +16,26 @@ function onScroll() {
     }
   });
 
-  // 2. ACTIVE LINK LOGIC
-  // Priority 1: If at the very bottom, highlight Contact
+  
   if (windowHeight + scrollPos >= bodyHeight - 100) {
     currentSectionId = "contact";
   } 
-  // Priority 2: If at the very top, highlight Home
+  
   else if (scrollPos < 150) {
     currentSectionId = "home";
   } 
-  // Priority 3: Middle of the page logic
+  
   else {
     sections.forEach(sec => {
       const rect = sec.getBoundingClientRect();
-      // Check if the section is occupying the middle of the viewport (y = 200px)
+      
       if (rect.top <= 200 && rect.bottom >= 200) {
         currentSectionId = sec.getAttribute("id");
       }
     });
   }
 
-  // 3. APPLY GLOW
+  
   if (currentSectionId) {
     links.forEach(link => {
       link.classList.remove("active");
